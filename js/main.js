66cdb199095416a01080ad8e1eb2c6994f3bd9d1
pato <patooodv@gmail.com>
2023-06-30 01:07:46 -0300

    se agrega buscador animado

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@
 
 let links = document.querySelectorAll(".link");
 
+let ancorBuscador = document.getElementById("ancorBuscador").addEventListener("click", (e)=>{
+    e.preventDefault();
+});
+
+let input = document.getElementById("inputBuscador");
+let liBuscador = document.getElementById("liBuscador")
+
+liBuscador.addEventListener("mouseover", ()=>{
+    input.classList.remove("esconderInput")
+    input.classList.add("expandirInput")
+})
+
+liBuscador.addEventListener("mouseleave", ()=>{
+    input.classList.add("esconderInput")
+    input.classList.remove("expandirInput")
+})
+
 console.log(links)
 function setearListeners() {
     for (let x of links) {
